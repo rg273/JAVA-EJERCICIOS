af862d276ec82639364c4fcddaf9027124642cda
rodrigod <rodrigogutierrez273@gmail.com>
2022-09-25 12:09:05 -0300

    Practica2 terminada

--- src/com/rodrigo/Practica2.java
+++ src/com/rodrigo/Practica2.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 
 public class Practica2 {
     public static void main(String[] args) {
+        //METODO 1
+        Scanner escanear = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        int numeroIntroducido = escanear.nextInt();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Introduce un numero: ");
-
-        int numero = scanner.nextInt();
-
-        double iva = numero * 0.21;
-
-        double suma = numero + iva;
-
-        System.out.println("El resultado con IVA es: " + suma);
-
+        double precioConIva = devolverIva(numeroIntroducido);
+        System.out.println(precioConIva);
+    }
+    static double devolverIva(int parametro1){
+        return parametro1 + (parametro1 * 0.20);
     }
 }
