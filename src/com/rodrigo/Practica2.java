bdb80eefd7058b66efe906ab028ca182cccd4edb
rodrigod <rodrigogutierrez273@gmail.com>
2022-09-25 11:44:02 -0300

    Practica2 terminada

---
+++ src/com/rodrigo/Practica2.java
@@ -0,0 +1,20 @@
+package com.rodrigo;
+import java.util.Scanner;
+
+public class Practica2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un numero: ");
+
+        int numero = scanner.nextInt();
+
+        double iva = numero * 0.21;
+
+        double suma = numero + iva;
+
+        System.out.println("El resultado con IVA es: " + suma);
+
+    }
+}
