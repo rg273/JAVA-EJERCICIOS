63bc6a408dd240ba0bf4ab9bb1db0f771260c3c5
rodrigod <rodrigogutierrez273@gmail.com>
2022-09-25 10:10:30 -0300

    Initial commit

---
+++ src/com/rodrigo/Practica1.java
@@ -0,0 +1,31 @@
+package com.rodrigo;
+
+public class Practica1 {
+    public static void main(String[] args) {
+        //NUMERICO
+        byte number = 123;
+        short number2 = 12345;
+        int number3 = 1234567890;
+        long number4 = 1234567890;
+
+        //punto flotante
+        double PI = 3.1423123123123123123d;
+        float e = 1.2123123123f;
+
+        //boolean
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //character
+        char character1 = 'a';
+
+        //CADENAS DE TEXTO
+        String nombre = "Rodriguito haciendo la suya";
+
+        //ENVOLTORIOS
+        Integer programing_A = null;
+
+        //comentario dummie
+
+    }
+}
